42de237baf60bc7d90ba2b6299fb5c8a463d881c
Osman Oliva <osmanoliva13@unitec.edu>
2015-05-29 08:42:12 -0600

    Update PPoTVisual.java

--- PiedraPapeloTijera/src/ppot/PPoTVisual.java
+++ PiedraPapeloTijera/src/ppot/PPoTVisual.java
@@ -14,7 +14,7 @@ public class PPoTVisual extends javax.swing.JFrame {
     }
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         jFrame1 = new javax.swing.JFrame();
@@ -43,6 +43,7 @@ public class PPoTVisual extends javax.swing.JFrame {
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         btn_piedra.setText("Piedra");
         btn_piedra.addActionListener(new java.awt.event.ActionListener() {
@@ -50,6 +51,7 @@ public class PPoTVisual extends javax.swing.JFrame {
                 btn_piedraActionPerformed(evt);
             }
         });
+        getContentPane().add(btn_piedra, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 208, 73, 67));
 
         btn_papel.setText("Papel");
         btn_papel.addActionListener(new java.awt.event.ActionListener() {
@@ -57,6 +59,7 @@ public class PPoTVisual extends javax.swing.JFrame {
                 btn_papelActionPerformed(evt);
             }
         });
+        getContentPane().add(btn_papel, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 286, 73, 67));
 
         btn_tijera.setText("Tijera");
         btn_tijera.addActionListener(new java.awt.event.ActionListener() {
@@ -64,27 +67,39 @@ public class PPoTVisual extends javax.swing.JFrame {
                 btn_tijeraActionPerformed(evt);
             }
         });
+        getContentPane().add(btn_tijera, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 364, 73, 67));
 
         lbl_piedra.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/piedra r.jpg"))); // NOI18N
+        getContentPane().add(lbl_piedra, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 210, 300, -1));
 
         lbl_papel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/papel 2.jpg"))); // NOI18N
         lbl_papel.setText("jLabel1");
         lbl_papel.setMaximumSize(new java.awt.Dimension(292, 250));
         lbl_papel.setMinimumSize(new java.awt.Dimension(292, 250));
         lbl_papel.setPreferredSize(new java.awt.Dimension(292, 250));
+        getContentPane().add(lbl_papel, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 210, -1, -1));
 
         lbl_tijera.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/tijera 2.jpg"))); // NOI18N
         lbl_tijera.setText("jLabel2");
+        getContentPane().add(lbl_tijera, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 210, 292, -1));
 
         lbl_piedra2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/piedra r.jpg"))); // NOI18N
+        getContentPane().add(lbl_piedra2, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 210, 320, 250));
 
         lbl_papel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/papel 2.jpg"))); // NOI18N
         lbl_papel2.setText("jLabel1");
+        getContentPane().add(lbl_papel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 180, 330, 310));
 
         lbl_tijera2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/tijera 2.jpg"))); // NOI18N
         lbl_tijera2.setText("jLabel2");
+        getContentPane().add(lbl_tijera2, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 210, 316, -1));
 
+        lbl_bloquear.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         lbl_bloquear.setText("Player 1");
+        getContentPane().add(lbl_bloquear, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 180, 150, -1));
+
+        lbl_ganador.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        getContentPane().add(lbl_ganador, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 80, 280, 47));
 
         btn_reiniciar.setText("Reiniciar");
         btn_reiniciar.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -97,73 +112,12 @@ public class PPoTVisual extends javax.swing.JFrame {
                 btn_reiniciarActionPerformed(evt);
             }
         });
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(btn_piedra, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btn_papel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(lbl_ganador, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btn_tijera, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btn_reiniciar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(lbl_bloquear, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(41, 41, 41)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(lbl_piedra)
-                        .addGap(18, 18, 18)
-                        .addComponent(lbl_piedra2, javax.swing.GroupLayout.PREFERRED_SIZE, 318, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(lbl_papel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(lbl_tijera, javax.swing.GroupLayout.PREFERRED_SIZE, 292, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lbl_tijera2, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(lbl_papel2, javax.swing.GroupLayout.PREFERRED_SIZE, 318, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addGap(180, 180, 180))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(176, 176, 176)
-                .addComponent(lbl_bloquear)
-                .addGap(18, 18, 18)
-                .addComponent(btn_piedra, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btn_papel, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btn_tijera, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(lbl_ganador, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btn_reiniciar, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(lbl_piedra)
-                    .addComponent(lbl_piedra2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbl_papel2)
-                    .addComponent(lbl_papel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbl_tijera2)
-                    .addComponent(lbl_tijera))
-                .addGap(0, 87, Short.MAX_VALUE))
-        );
+        getContentPane().add(btn_reiniciar, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 450, -1, 67));
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }// </editor-fold>                        
 
-    private void btn_piedraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_piedraActionPerformed
+    private void btn_piedraActionPerformed(java.awt.event.ActionEvent evt) {                                           
     
     if(lbl_bloquear.getText().equals("Player 1")){    
     //Scanner chooseUrDestiny = new Scanner(System.in);
@@ -195,13 +149,13 @@ public class PPoTVisual extends javax.swing.JFrame {
             lbl_ganador.setText("El Ganador fue la computadora!");
         } else if (computer_choice == 3){
             lbl_ganador.setText("El Ganador fuiste tu!");
+
         }
-        lbl_bloquear.setText("Bloquear");
-
+           lbl_bloquear.setText("Terminado");
     }
-    }//GEN-LAST:event_btn_piedraActionPerformed
+    }                                          
 
-    private void btn_tijeraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_tijeraActionPerformed
+    private void btn_tijeraActionPerformed(java.awt.event.ActionEvent evt) {                                           
     
     if(lbl_bloquear.getText().equals("Player 1")){
     //Scanner chooseUrDestiny = new Scanner(System.in);
@@ -234,11 +188,11 @@ public class PPoTVisual extends javax.swing.JFrame {
         } else if (computer_choice == 3){
             lbl_ganador.setText("Empatados!");
         }
+         lbl_bloquear.setText("Terminado");
     }  
-        lbl_bloquear.setText("Bloquear");
-    }//GEN-LAST:event_btn_tijeraActionPerformed
+    }                                          
 
-    private void btn_papelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_papelActionPerformed
+    private void btn_papelActionPerformed(java.awt.event.ActionEvent evt) {                                          
     
     if(lbl_bloquear.getText().equals("Player 1")){
     //Scanner chooseUrDestiny = new Scanner(System.in);
@@ -272,152 +226,29 @@ public class PPoTVisual extends javax.swing.JFrame {
         } else if (computer_choice == 3){
             lbl_ganador.setText("El Ganador fue la computadora!");
         }
+         lbl_bloquear.setText("Terminado");
     }
-    lbl_bloquear.setText("Bloquear");
-    }//GEN-LAST:event_btn_papelActionPerformed
+    }                                         
 
-    private void btn_reiniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_reiniciarActionPerformed
-        if(lbl_bloquear.getText().equals("Bloquear")){
+    private void btn_reiniciarActionPerformed(java.awt.event.ActionEvent evt) {                                              
+        if(lbl_bloquear.getText().equals("Terminado")){
         lbl_piedra.setVisible(false);
         lbl_papel.setVisible(false);
         lbl_tijera.setVisible(false);
         lbl_piedra2.setVisible(false);
         lbl_papel2.setVisible(false);
         lbl_tijera2.setVisible(false);
-        lbl_ganador.setText("");
+        lbl_ganador.setText("Juego Reiniciado");
         lbl_bloquear.setText("Player 1");
         }
-    }//GEN-LAST:event_btn_reiniciarActionPerformed
+    }                                             
 
-    private void btn_reiniciarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_reiniciarMouseClicked
+    private void btn_reiniciarMouseClicked(java.awt.event.MouseEvent evt) {                                           
         // TODO add your handling code here:
-    }//GEN-LAST:event_btn_reiniciarMouseClicked
+    }                                          
 
     public static void main(String args[]) {
-       /* Scanner chooseUrDestiny = new Scanner(System.in);
-		Random rng_jesus = new Random(0);
-		int destiny;//seleccion de jugador
-		String otravez;
-                lbl_piedra.setVisible(false);
-		do
-		{
 
-			//Jugador escoge su destino…
-			while (true)
-			{			
-				System.out.print("\nSeleccione: \n1.Piedra \n2.Papel \n3.Tijera? \nSelección: ");
-				destiny = chooseUrDestiny.nextInt();
-				if (destiny == 1 || destiny == 2 || destiny ==3)
-					break;
-				else
-					System.out.println("Selección inválida.");//crashea si ingresa texto, no números. :/ oh well.
-
-			}
-			// fin de selección
-
-			//Strings que le dan nombres a las selecciones
-			String realdestiny = "";//jugador
-			String computerdestiny = "";//computadora
-
-			//la computadora escoge su destino…
-			int computer_choice;
-			computer_choice = rng_jesus.nextInt(3)+1;
-
-			//Se le asigna nombre a la selección de la computadora
-			switch (computer_choice)
-			{
-				case 1:
-				computerdestiny = "Piedra";
-				break;
-
-				case 2:
-				computerdestiny = "Papel";
-				break;
-
-				case 3:
-				computerdestiny = "Tijera";
-				break;
-
-			}
-			System.out.print("\n\n");//separa el texto cause muh aesthetics
-
-			//asigna nombre a selección de jugador y define el ganador.
-			switch (destiny)
-			{
-				case 1://jugador escoge piedra
-					realdestiny = "Piedra";
-					System.out.println("Ha escogido: "+realdestiny+".");
-					System.out.println("La computadora ha escogido: "+computerdestiny+".");
-					switch (computer_choice)
-					{
-						case 1: //	ai escoge piedra
-							System.out.println("Empate.");
-							break;
-						case 2: //	ai escoge papel
-							System.out.println("Gana la computadora.");
-							break;
-						case 3: //	ai escoge tijera
-							System.out.println("Gana el jugador.");
-							break;	
-
-					}	
-						break;
-
-
-				case 2://jugador escoge papel
-					realdestiny = "Papel";
-					System.out.println("Ha escogido: "+realdestiny+".");
-					System.out.println("La computadora ha escogido: "+computerdestiny+".");
-					switch (computer_choice)
-					{
-						case 1: //	ai escoge piedra
-							System.out.println("Gana el jugador.");
-							break;
-						case 2: //	ai escoge papel
-							System.out.println("Empate.");
-							break;
-						case 3: //	ai escoge tijera
-							System.out.println("Gana la computadora.");
-							break;	
-
-					}
-					break;
-
-
-				case 3://jugador escoge tijera
-					realdestiny = "Tijera";
-					System.out.println("La computadora ha escogido: "+computerdestiny+".");
-					System.out.println("Ha escogido: "+realdestiny+".");	
-
-					switch (computer_choice)
-					{
-						case 1: //	ai escoge piedra
-							System.out.println("Gana la computadora.");
-							break;
-						case 2: //	ai escoge papel
-							System.out.println("Gana el jugador.");
-							break;
-						case 3: //	ai escoge tijera
-							System.out.println("Empate.");
-							break;	
-					}
-					break;
-			}
-			//¿otra vez?
-			while(true)
-			{
-				System.out.print("\nOtra vez? "); 
-				otravez = chooseUrDestiny.next().toUpperCase();
-				if(!otravez.equals("SI") && !otravez.equals("NO"))
-					System.out.print("Selección inválida.\n");
-				else
-					break;
-			}
-
-		}while (otravez.equals("SI"));
-		//fin del "otravez"	
-	}
-}*/
 
 
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -449,7 +280,7 @@ public class PPoTVisual extends javax.swing.JFrame {
         });
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // Variables declaration - do not modify                     
     private javax.swing.JButton btn_papel;
     private javax.swing.JButton btn_piedra;
     private javax.swing.JButton btn_reiniciar;
@@ -463,6 +294,8 @@ public class PPoTVisual extends javax.swing.JFrame {
     private javax.swing.JLabel lbl_piedra2;
     private javax.swing.JLabel lbl_tijera;
     private javax.swing.JLabel lbl_tijera2;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration                   
 }
+
+
 
